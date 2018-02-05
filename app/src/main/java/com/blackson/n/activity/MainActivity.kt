f62442fa648251ff7cee58ede9c5e68d207758cd
neville <nevilleblackson@gmail.com>
2018-02-05 13:04:53 +0100

    start project

---
+++ app/src/main/java/com/blackson/n/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.blackson.n.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.blackson.n.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        nextButton.setOnClickListener {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/blackson/n/utils/Utils.kt
@@ -0,0 +1,13 @@
+package com.blackson.n.utils
+
+/***
+ * Created by n on 5-2-18.
+ */
+class Utils {
+
+    companion object {
+        public fun dpToPixles(dp: Int): Int {
+            return dp
+        }
+    }
+}
